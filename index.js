400e83dab3bf0c4d80953d9f581f3e33ae88306d
Lisa <wamaithak05@gmail.com>
2024-03-29 14:31:17 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity="roller-skate") {
+    return 'This Saturday, I want to roller-skate!'
+}
+
+let mondayWork = function(actvity="go to the office") {
+    return 'This Monday, I will go to the office.'
+}
+
+let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+        return "You are *a dedicated programmer*"
+    }
+}
